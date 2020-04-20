9f5c4c3e4e97fc1d95e2725fc78dfb6ae0948904
Muhamad Irham Prasetyo <muhamad.prasetyo@get-ag.com>
2020-04-21 00:53:42 +0200

    add cors anywhere

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
 				const data = { ...this.state };
 				
 				axios
-						.post("https://live-coding-server.now.sh/update-editor", data)
+						.post("https://cors-anywhere.herokuapp.com/https://live-coding-server.now.sh/update-editor", data)
 						.catch(console.error);
 		};
     
